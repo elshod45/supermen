be14a36c34a10d829182d2981cc429f4e3d0fa5e
Elshod <elshod45>
2024-03-09 21:35:21 +0500

    homework 1 Fixes #1

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelector("#heading").textContent = "Привет Элшод"
+
+
+console.log("Привет мир!");
+
+console.info("Это вывод информации");
+
+console.warn("это предупреждение");
+
+console.error("произошла ошибка");
+
+console.log(3 + 4 - 7 + 1 + 2 * 3 / 2);
+
+const variable1 = 4
+
+let variable2 = 5
+
+var variable3 = 6
+
+
+console.log(variable1 + variable2);
+
+
+let val1 = document.querySelector("#value1").value
+let val2 = document.querySelector("#value2").value
+
+let output = +val1 + +val2
+
+console.log(output);
+
+Document.querySelector("#output").textContent = "output"
